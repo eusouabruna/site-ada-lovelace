3d5402cf5585d287dfe9f31d072e60b6ab422214
eusouabruna <brunapedrosaguedes@outlook.com>
2025-07-20 19:12:52 -0300

    mudei estilo e js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
 function validaFormulario () {
-    if( document.getElementById("nome").value != "" &&
-    document.getElementById("email").value != "" &&
-    document.getElementById("telefone").value != ""){
+    // Criação das variáveis para armazenar os valores dos campos
+    var nome = document.getElementById("nome").value
+    var email = document.getElementById("email").value
+    var telefone = document.getElementById("telefone").value
+
+    // Verificação se todos os campos foram preenchidos
+    if(nome != "" && email != "" && telefone != ""){
         alert("Prontinho! Você receberá as novidades por email.")
     }else{
         alert("Por favor, preencha os campos nome, email e telefone!")
